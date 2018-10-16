3293e4025fc8077ce05765125f8f18ce9c90b857
Michael Carleton <micycle1@hotmail.co.uk>
2018-10-16 13:46:47 +0100

    q5

--- q5.py
+++ q5.py
@@ -1,8 +1,11 @@
-# ONLY EDIT FUNCTIONS MARKED CLEARLY FOR EDITING
-
-import numpy as np
-
 def question05(allowedAllocations, totalValue):
-  # modify and then return the variable below
-  answer = -1
-  return answer
+    table = [0] #base
+    for i in range(totalValue):
+        table.append(sys.maxsize)
+    for i in range(1, totalValue+1):
+        for j in range(len(allowedAllocations)):
+            if allowedAllocations[j] <= i:
+                sub_res = table[i - allowedAllocations[j]]
+                if (sub_res != sys.maxsize and (sub_res + 1) < table[i]):
+                    table[i] = sub_res+1
+    return table[-1]
