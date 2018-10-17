a5a68949624c5268ca4467a30442ca07e32f3814
Michael Carleton <micycle1@hotmail.co.uk>
2018-10-17 11:11:08 +0100

    fix q4?

--- q4.py
+++ q4.py
@@ -1,34 +1,33 @@
-# ONLY EDIT FUNCTIONS MARKED CLEARLY FOR EDITING
-
-#import numpy as np
-import itertools
-
-#compute all combinations for two portfolios
 def question04(rows, numberMachines):
 
-  def generate():
-    sequences = []
-    for i, row in enumerate(rows):
-      temp = []
-      for k, symbol in enumerate(row):
-        if symbol != 'X':
-          temp.append(symbol)
-        else:
-          if len(temp) >= numberMachines:
-            sequences.append(temp)
-            #yield temp
-            temp = []
-    return sequences
+  if not any(isinstance(i, list) for i in rows):
+    rows = [rows]
 
-  out = generate()
-  if not out: # empty
+  sequences = []
+  for i, row in enumerate(rows):
+    temp = []
+    for k, symbol in enumerate(row):
+      if symbol != 'X':
+        temp.append(symbol)
+      else:
+        if len(temp) >= numberMachines:
+          sequences.append(temp)
+          temp = []
+    if temp and len(temp) >= numberMachines:
+      sequences.append(temp) #still append if no 'x' found
+      temp = []
+
+  if not sequences: # empty
     return 0
 
-  max_sum = 2**64
-  for sequence in out:
-    sequence = list(itertools.combinations(sequence, numberMachines))
-    for subsequence in sequence:
-      max_sum = min(max_sum, sum(subsequence))
+  max_sum = 2**32
+  for sequence in sequences:
+    #sequence = list(itertools.combinations(sorted(sequence), numberMachines))
+    sequence = sorted(sequence)
+    sequenceSum = sum(sequence[i] for i in range(numberMachines))
+    max_sum = min(max_sum, sequenceSum)
+##    for subsequence in sequence:
+##      max_sum = min(max_sum, sum(subsequence))
   return max_sum
 
 """
@@ -41,4 +40,11 @@ test = ([11,12, 12, 3, 'X', 3],
 test2 = (['X', 'X', 2], [2, 3, 'X'], ['X', 3, 2])
 
 test3 = ([2, 3, 'X', 2], [4, 'X', 'X', 4], [3, 2, 'X', 'X'], ['X', 'X', 'X', 5])
+
+
+test4 = ([11,12, 12, 3, 'X', 3],[23, 'X', 'X', 'X', 3],[23, 'X', 'X', 'X', 3],
+        [23, 'X', 'X', 'X', 3, 33, 21, 'X', 'X', 'X', 21, 'X', 'X', 7, 9, 2],
+        [33, 21, 'X', 'X', 'X'],[9, 12, 3, 'X', 'X','X', 4, 5, 423,4,3,3,5,6],['X', 'X', 'X', 4, 5],['X', 'X', 'X', 4, 5],[9, 12, 3, 'X', 'X','X', 4, 5,423,4,3,3,5,6],['X', 'X', 'X', 4, 5],['X', 'X', 'X', 4, 5],
+        [9, 12, 3, 'X', 'X','X', 4, 5,423,4,3,3,5,6],['X', 'X', 'X', 4, 5],['X', 'X', 'X', 4, 5],
+        ['X', 'X', 'X', 4, 5])
 """
